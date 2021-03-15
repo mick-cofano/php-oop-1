936435c7169d9e317a157a5aad07070ed2f669d3
Michele Cofano <brutalmachinelol@gmail.com>
2021-03-15 16:30:02 +0100

    rifatto in inglese

--- index.php
+++ index.php
@@ -1,37 +1,38 @@
 <?php
 
-  class Automobile {
+  class Car {
 
-    public $marca;
+    public $brand;
 
-    public $modello;
+    public $model;
 
-    public $prezzo;
+    public $price;
 
 
-    public function __construct($marca, $modello, $prezzo) {
-      $this->marca = $marca;
-      $this->modello = $modello;
-      $this->prezzo = $prezzo;
+    public function __construct($brand, $model, $price) {
+      $this->brand = $brand;
+      $this->model = $model;
+      $this->price = $price;
     }
+
   }
 
-  $ford = new Automobile('Ford', 'Fiesta', '19500€');
-    echo $ford->marca . '<br/>';
-    echo $ford->modello . '<br/>';
-    echo $ford->prezzo . '<br/>' . '<br/>' . '<br/>';
-  $ferrari = new Automobile('Ferrari', 'SF-90 Stradale', '515000€');
-    echo $ferrari->marca . '<br/>';
-    echo $ferrari->modello . '<br/>';
-    echo $ferrari->prezzo . '<br/>' . '<br/>' . '<br/>';
-  $jeep = new Automobile('Jeep', 'Wrangler', '49500€');
-    echo $jeep->marca . '<br/>';
-    echo $jeep->modello . '<br/>';
-    echo $jeep->prezzo . '<br/>' . '<br/>' . '<br/>';
-  $chevrolet = new Automobile('Chevrolet', 'Corvette C8', '158000€');
-    echo $chevrolet->marca . '<br/>';
-    echo $chevrolet->modello . '<br/>';
-    echo $chevrolet->prezzo . '<br/>' . '<br/>' . '<br/>';
+  $ford = new Car('Ford', 'Fiesta', '19500€');
+    echo $ford->brand . '<br/>';
+    echo $ford->model . '<br/>';
+    echo $ford->price . '<br/>' . '<br/>' . '<br/>';
+  $ferrari = new Car('Ferrari', 'SF-90 Stradale', '515000€');
+    echo $ferrari->brand . '<br/>';
+    echo $ferrari->model . '<br/>';
+    echo $ferrari->price . '<br/>' . '<br/>' . '<br/>';
+  $jeep = new Car('Jeep', 'Wrangler', '49500€');
+    echo $jeep->brand . '<br/>';
+    echo $jeep->model . '<br/>';
+    echo $jeep->price . '<br/>' . '<br/>' . '<br/>';
+  $chevrolet = new Car('Chevrolet', 'Corvette C8', '158000€');
+    echo $chevrolet->brand . '<br/>';
+    echo $chevrolet->model . '<br/>';
+    echo $chevrolet->price . '<br/>' . '<br/>' . '<br/>';
 
 
  ?>
