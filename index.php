eccb16e0450bf5860dad5ab1f6d33df6f5f346ed
Michele Cofano <brutalmachinelol@gmail.com>
2021-03-15 16:52:12 +0100

    fatta seconda parte dell'esercizio

--- index.php
+++ index.php
@@ -17,22 +17,52 @@
 
   }
 
+  class Dealer {
+
+    public $name;
+
+    private $cars = [];
+
+    public function __construct($name) {
+      $this->name = $name;
+    }
+
+    public function addCar(Car $car) {
+      $this->cars[] = $car;
+    }
+
+    public function getCars() {
+      return $this->cars;
+    }
+
+  }
+
   $ford = new Car('Ford', 'Fiesta', '19500€');
-    echo $ford->brand . '<br/>';
-    echo $ford->model . '<br/>';
-    echo $ford->price . '<br/>' . '<br/>' . '<br/>';
+    // echo $ford->brand . '<br/>';
+    // echo $ford->model . '<br/>';
+    // echo $ford->price . '<br/>' . '<br/>' . '<br/>';
   $ferrari = new Car('Ferrari', 'SF-90 Stradale', '515000€');
-    echo $ferrari->brand . '<br/>';
-    echo $ferrari->model . '<br/>';
-    echo $ferrari->price . '<br/>' . '<br/>' . '<br/>';
+    // echo $ferrari->brand . '<br/>';
+    // echo $ferrari->model . '<br/>';
+    // echo $ferrari->price . '<br/>' . '<br/>' . '<br/>';
   $jeep = new Car('Jeep', 'Wrangler', '49500€');
-    echo $jeep->brand . '<br/>';
-    echo $jeep->model . '<br/>';
-    echo $jeep->price . '<br/>' . '<br/>' . '<br/>';
+    // echo $jeep->brand . '<br/>';
+    // echo $jeep->model . '<br/>';
+    // echo $jeep->price . '<br/>' . '<br/>' . '<br/>';
   $chevrolet = new Car('Chevrolet', 'Corvette C8', '158000€');
-    echo $chevrolet->brand . '<br/>';
-    echo $chevrolet->model . '<br/>';
-    echo $chevrolet->price . '<br/>' . '<br/>' . '<br/>';
+    // echo $chevrolet->brand . '<br/>';
+    // echo $chevrolet->model . '<br/>';
+    // echo $chevrolet->price . '<br/>' . '<br/>' . '<br/>';
+
+
+    $newDealer = new Dealer('Mick Cofano Dealer');
+    $newDealer->addCar($ford);
+    $newDealer->addCar($ferrari);
+    $newDealer->addCar($jeep);
+    $newDealer->addCar($chevrolet);
+
+    var_dump($newDealer);
+
 
 
  ?>
